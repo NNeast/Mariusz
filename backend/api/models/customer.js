2c8095e1675f6ac569a3ec09d99af2bb666b100d
Bigman <zawislaktomasz4@gmail.com>
2023-05-28 22:56:53 +0200

    added basic customer module

---
+++ backend/api/models/customer.js
@@ -0,0 +1,16 @@
+const { Schema, model } = require('mongoose')
+
+const customerSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    surname: {
+        type: String,
+        required: true,
+    }
+})
+
+const Customer = model('Customer', customerSchema)
+
+module.exports = Customer
